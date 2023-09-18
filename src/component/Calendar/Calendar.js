bb66556bd5dfae84c91d03900b313b0a4efeb63f
afsaints0987 <saints0987@gmail.com>
2023-09-19 01:59:35 +0800

    adjust the days of the month

--- src/component/Calendar/Calendar.js
+++ src/component/Calendar/Calendar.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import '../Calendar/Calendar.css'
-
+import React from "react";
+import "../Calendar/Calendar.css";
 
 const Calendar = () => {
-  const [date, setDate] = React.useState(new Date())
-  const [days, setDays] = React.useState([])
-  const wdays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']
+  const [date, setDate] = React.useState(new Date());
+  const [days, setDays] = React.useState([]);
+  const wdays = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
 
   // Get the previous month of the year
   const prevMonth = () => {
@@ -14,43 +13,54 @@ const Calendar = () => {
 
   // Get the next month of the year
   const nextMonth = () => {
-    setDate(new Date(date.getFullYear(), date.getMonth() + 1))
-  }
+    setDate(new Date(date.getFullYear(), date.getMonth() + 1));
+  };
 
   // Get the days of the month this year
   const getDays = () => {
-    const firstDay = new Date(date.getFullYear(), (date.getMonth() + 1 )- 1, 1)
-    const lastDay = new Date(date.getFullYear(), (date.getMonth() + 1), 0)
+    const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    const startingDayIndex = firstDay.getDay(); // 0 (Sunday) to 6 (Saturday)
 
-    const daysInMonth = []
-    let currentDay = firstDay;
+    const daysInMonth = [];
+    let currentDay = 1;
 
-    while(currentDay <= lastDay) {
-      daysInMonth.push(currentDay.getDate());
-      currentDay.setDate(currentDay.getDate() + 1)
+    // Add the remaining days of the previous month
+    for (let i = 0; i < startingDayIndex; i++) {
+      daysInMonth.push(null); // Use null to represent empty cells
     }
 
-    setDays(daysInMonth)
-  }
+    // Add days of the current month
+    while (currentDay <= lastDay.getDate()) {
+      daysInMonth.push(currentDay);
+      currentDay++;
+    }
+
+    setDays(daysInMonth);
+  };
 
   React.useEffect(() => {
-    getDays()
+    getDays();
 
     return () => {
-      setDays([])
-    }
-  },[date])
+      setDays([]);
+    };
+  }, [date]);
 
-  console.log(days)
+  console.log(days);
 
   return (
     <div className="calendar">
       <div className="calendar-header">
-        <button className="caret left" onClick={prevMonth}>&#60;</button>
+        <button className="caret left" onClick={prevMonth}>
+          &#60;
+        </button>
         <h5>
           {date.toLocaleString("en-us", { month: "long", year: "numeric" })}
         </h5>
-        <button className="caret right" onClick={nextMonth}>&#62;</button>
+        <button className="caret right" onClick={nextMonth}>
+          &#62;
+        </button>
       </div>
       <table className="calendar-table">
         <thead className="table-header">
@@ -61,15 +71,15 @@ const Calendar = () => {
           ))}
         </thead>
         <tbody className="table-body">
-            {days.map((day, index) => (
-              <tr key={index}>
-                <td>{day}</td>
-              </tr>
-            ))}
+          {days.map((day, index) => (
+            <tr key={index}>
+              <td>{day}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
   );
-}
+};
 
-export default Calendar
+export default Calendar;
